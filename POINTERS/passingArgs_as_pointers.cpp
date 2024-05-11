d929811f3cd22b7d4465dbd2274739330298aac2
Lennox Mountain <lennoxmountain67@gmail.com>
2024-05-11 22:14:36 +0200

    swapping variables

--- POINTERS/passingArgs_as_pointers.cpp
+++ POINTERS/passingArgs_as_pointers.cpp
@@ -28,3 +28,28 @@ void swapBy_reference(int& value1, int& value2){
  * swapBy_pointer: swaps by pointer
 */
 
+void swapBy_pointer(int* value1, int* value2){
+    int* temp = value1;
+    value1 = value2;
+    value2 = temp;
+}
+
+/**
+ * Main: tests the results 
+*/
+
+int main(){
+    int num1 = 5; 
+    int num2 = 10;
+
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+
+    cout<<"Value of num1 before swap: "<<num1<<endl;
+    cout<<"Value of num2 before swap: "<<num2<<endl;
+
+    // swapBy_value(num1, num2);
+    // cout<<"Value of num1 after swap: "<<num1<<endl;
+    // cout<<"Value of num2 after swap: "<<num2<<endl;
+}
