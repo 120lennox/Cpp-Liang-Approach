b33c5ef3866d29ad3ee0f59c1825873fb2c19c8f
Lennox Mountain <lennoxmountain67@gmail.com>
2024-05-12 11:46:02 +0200

    adding program implementation in Main.cpp ready for running

---
+++ POINTERS/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Student.H"
+using namespace std;
+
+int main(){
+    Student student1;
+
+    student1.fName = "Lennox";
+    student1.lName = "Mt";
+    
+    cout<<"Hello "<<student1.fName<<", Choose from the following options:"<<endl;
+    cout<<"1. "
+}
